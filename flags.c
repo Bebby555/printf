6c529ed5513b2e9ab218de0410da115771ddde13
gozie8 <138773276+gozie8@users.noreply.github.com>
2023-09-22 13:27:39 +0100

    Update flags.c
    Gozie and Deborah

--- flags.c
+++ flags.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * By Deborah and Gozie
+ * By Deborah and Gozie for printf
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
