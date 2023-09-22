22091e3298187e48fb332771be3d793b5c100c95
gozie8 <138773276+gozie8@users.noreply.github.com>
2023-09-22 13:30:23 +0100

    Update get_width.c
    Get_width.c task

--- get_width.c
+++ get_width.c
@@ -2,7 +2,7 @@
 
 /**
  * get_width - Calculates the width for printing
- * By gozie and deborah
+ * By gozie and deborah for get_width
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
