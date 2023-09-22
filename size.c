3138ec00c52b66a556e32b963c593b02d848729b
gozie8 <138773276+gozie8@users.noreply.github.com>
2023-09-22 13:38:26 +0100

    Update size.c
    size c task by Deborah and Gozie

--- size.c
+++ size.c
@@ -2,7 +2,7 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * By Deborah and Gozie
+ * By Deborah and Gozie for size.c
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
