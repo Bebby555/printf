7b9ffc9effbdfdda11b2fe0b2ec0f07d9076b3fd
gozie8 <138773276+gozie8@users.noreply.github.com>
2023-09-22 13:36:53 +0100

    Update print_functions1.c
    print_function1.c task by Deborah and Gozie

--- print_functions1.c
+++ print_functions1.c
@@ -4,7 +4,7 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * By Deborah and gozie
+ * By Deborah and gozie for print_functions1.c
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
